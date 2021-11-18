3bac732a77b937f46f4d5c5a8b6a1529acd51f6f
Jonathan <jonxplod@outlook.fr>
2021-11-18 23:56:53 +0100

    Fin de la responsive

--- public/js/main.js
+++ public/js/main.js
@@ -1,15 +1,27 @@
 // Mes Selecteurs
+let navMenu = document.querySelector(".nav-list");
+let btnMenu = document.querySelector(".btn-menu");
+let btnCancel = document.querySelector(".btn-x");
 let backgroundBlack = document.querySelector(".btnBlack");
 let backgroundWhite = document.querySelector(".btnBlanc");
 let a = document.querySelectorAll("a");
 
-// Mes events
+// Event Navbar
+btnMenu.addEventListener("click", () => {
+    navMenu.classList.add("active");
+})
+btnCancel.addEventListener("click", () => {
+    navMenu.classList.remove("active");
+})
+
+// Mes events btn Black et White
 backgroundBlack.addEventListener("click", () => {
     document.body.style.backgroundColor = "black"
     document.body.style.color = "white"
     for (i = 0; i < a.length; i++) {
         a[i].style.color = 'white';
     }
+    document.querySelector(".btn-menu").style.color = "white"
     document.querySelector(".sc2P").style.color = "black"
     document.querySelector(".row11 > p").style.color = "black"
     document.querySelector(".row11 > h6").style.color = "black"
@@ -18,11 +30,12 @@ backgroundBlack.addEventListener("click", () => {
     document.querySelector(".row33 > h6").style.color = "black"
     document.querySelector(".row33 > i").style.color = "black"
 })
-
 backgroundWhite.addEventListener("click", () => {
     document.body.style.backgroundColor = "white"
     document.body.style.color = "black"
+    document.querySelector(".btn-menu").style.color = "black"
     for (i = 0; i < a.length; i++) {
         a[i].style.color = 'black';
     }
 })
+
