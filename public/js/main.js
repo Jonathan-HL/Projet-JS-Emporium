11ed4b1ef4611e8a3a0ea952db7848e21c26d886
Jonathan <jonxplod@outlook.fr>
2021-11-22 00:20:23 +0100

    Fin btn Conex

--- public/js/main.js
+++ public/js/main.js
@@ -5,7 +5,13 @@ let btnCancel = document.querySelector(".btn-x");
 let backgroundBlack = document.querySelector(".btnBlack");
 let backgroundWhite = document.querySelector(".btnBlanc");
 let a = document.querySelectorAll("a");
-
+let conex = document.querySelector(".conex")
+let loginForm  = document.querySelector("#formLogin")
+let forLoginn = document.querySelector("#log")
+let forInscrip = document.querySelector("#formInscrip")
+let forRegistre = document.querySelector("#inscrip")
+let formContainer = document.querySelector("#formContainer")
+let cancelPopup = document.querySelector(".icon-x")
 // Mes Functions
 let aBlack = () => {
     a.forEach(el => {
@@ -24,6 +30,10 @@ btnMenu.addEventListener("click", () => {
     navMenu.classList.add("active");
     backgroundBlack.addEventListener("click", () => {
         aRed()
+        document.querySelector(".btn-x").style.color = "#DD4545"
+    })
+    backgroundWhite.addEventListener("click", () => {
+        document.querySelector(".btn-x").style.color = "black"
     })
 })
 btnCancel.addEventListener("click", () => {
@@ -37,7 +47,7 @@ backgroundBlack.addEventListener("click", () => {
     document.body.style.backgroundColor = "black"
     document.body.style.color = "#DD4545"
     document.querySelector("h1").style.color = "#DD4545"
-    document.querySelector(".btn-menu").style.color = "white"
+    document.querySelector(".btn-menu").style.color = "grey"
     document.querySelector(".sc2P").style.color = "black"
     document.querySelector(".row11 > p").style.color = "black"
     document.querySelector(".row11 > h6").style.color = "black"
@@ -59,4 +69,35 @@ backgroundWhite.addEventListener("click", () => {
 window.addEventListener("scroll", () => {
     let navScroll = document.querySelector("nav")
         navScroll.classList.toggle("sticky", window.scrollY > 200)
+})
+
+// Event popUp Conex / Inscription
+conex.addEventListener("click", () => {
+    document.querySelector(".form-wrapper .card").classList.toggle("show")
+})
+
+cancelPopup.addEventListener("click", () => {
+    document.querySelector(".form-wrapper .card").classList.remove("show")
+})
+
+forLoginn.addEventListener("click", () => {
+    forLoginn.classList.add("active")
+    forRegistre.classList.remove("active")
+    if (loginForm.classList.contains("toggleForm")) {
+            formContainer.style.transform = `translate(0%)`;
+            formContainer.style.transition = "transform 0.5s";
+            forInscrip.classList.add("toggleForm");
+            loginForm.classList.remove("toggleForm");
+    }
+})
+
+forRegistre.addEventListener("click", () => {
+    forLoginn.classList.remove("active")
+    forRegistre.classList.add("active")
+    if (forInscrip.classList.contains("toggleForm")) {
+            formContainer.style.transform = `translate(-100%)`;
+            formContainer.style.transition = "transform 0.5s";
+            forInscrip.classList.remove("toggleForm");
+            loginForm.classList.add("toggleForm");
+    }
 })
